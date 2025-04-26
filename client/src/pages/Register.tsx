eb4aef6bfeffc01f405a103d2f61de7959046c8d
Enoch Garoli <eenkiga@gmail.com>
2025-04-26 14:24:53 +0300

    Created the Registration Screen

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,15 @@
+import RegistrationForm from "@/components/RegistrationForm";
+
 type Props = {};
 
 const Register = ({}: Props) => {
-  return <div>Register Page</div>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <RegistrationForm />
+      </div>
+    </div>
+  );
 };
 
 export default Register;
