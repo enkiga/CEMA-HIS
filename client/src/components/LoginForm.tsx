0663cf792c324f2e45af65de3229a5424fc3e7d7
Enoch Garoli <eenkiga@gmail.com>
2025-04-26 14:16:33 +0300

    Created the Login Screen

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,116 @@
+import { cn } from "@/lib/utils";
+import { Card, CardContent } from "@/components/ui/card";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+type Props = {
+  className?: string;
+  props?: React.ComponentProps<"div">;
+};
+
+// Login Zod Schema
+const formSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+const LoginForm = ({ className, ...props }: Props) => {
+  // Define our login form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  //   Defining our submit function
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    console.log("Form submitted", data);
+    // Here you would typically send the data to your server
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden">
+        <CardContent className="grid px-0 md:grid-cols-2">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-8 p-6 md:p-8"
+            >
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <p className="text-balance text-muted-foreground">
+                  Login to account
+                </p>
+              </div>
+
+              {/* Our form fields here */}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="doctor@gmail.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full md:w-fit">Login</Button>
+
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div>
+            </form>
+          </Form>
+          <div className="relative hidden bg-muted md:block md:col-span-1 mr-5 rounded-sm">
+            <div className="absolute inset-0 bg-gradient-to-b from-muted to-transparent z-10 rounded-t-sm" />
+            <img
+              src="https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale rounded-sm"
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default LoginForm;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,16 @@
-type Props = {}
+import LoginForm from "@/components/LoginForm";
+
+type Props = {};
 
 const Login = ({}: Props) => {
   return (
-    <div>Login Page</div>
-  )
-}
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+      
+    </div>
+  );
+};
 
-export default Login
+export default Login;
